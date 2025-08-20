8d5df80df1efd156d674ea01a899901d704ac188
emanessam <emanessam05@gmail.com>
2025-08-20 19:26:43 +0100

    add reverseString add mergeSortedArrays

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,67 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+
+    val myArray = MyArray<Int>()
+    println(myArray.length)
+
+    myArray.push(1)
+    myArray.push(2)
+    myArray.printArray()
+    myArray.pop()
+    myArray.pop()
+    myArray.pop()
+    myArray.push(4)
+    myArray.push(5)
+    myArray.push(6)
+    myArray.push(7)
+    myArray.printArray()
+    myArray.delete(1)
+    myArray.printArray()
+    println(myArray.length)
+    println(reverseString("eman"))
+    println(mergeSortedArrays(intArrayOf(), intArrayOf(4, 5, 6)).contentToString())
 }
+
+fun reverseString(s: String): String {
+    var result = StringBuilder(s.length)
+    for (i in s.length - 1 downTo 0) {
+        result.append(s[i])
+    }
+    return result.toString()
+
+}
+
+fun mergeSortedArrays(array1: IntArray, array2: IntArray): IntArray {
+
+    val newArray = IntArray(size = array1.size + array2.size)
+    var i = 0
+    var j = 0
+    var k = 0
+
+
+    if (array1.isEmpty()) return array2
+    if (array2.isEmpty()) return array1
+
+    while (i < array1.size && j < array2.size) {
+
+        if (array1[i] < array2[j]) {
+            newArray[k++] = array1[i++]
+        } else {
+            newArray[k++] = array2[j++]
+        }
+    }
+
+    while (i < array1.size) {
+        newArray[k++] = array1[i++]
+    }
+    while (j < array2.size) {
+        newArray[k++] = array2[j++]
+    }
+
+
+    return newArray
+}
+
+
--- src/MyArray.kt
+++ src/MyArray.kt
@@ -1,18 +1,17 @@
 class MyArray<T>(
     private var arr: Array<Any?> = arrayOfNulls(1),
-    private var capacity: Int = 0,
+    private var capacity: Int = 1,
     var length: Int = 0
 ) {
 
     @Suppress("UNCHECKED_CAST")
-    fun get(index: Int): T {
+    operator fun get(index: Int): T {
         if (index in 0 until length) return arr[index] as T
         throw IndexOutOfBoundsException()
     }
 
 
     fun push(item: T) {
-
         if (capacity == length) {
             val newArr = arrayOfNulls<Any>(capacity * 2)
 
@@ -20,15 +19,34 @@ class MyArray<T>(
                 newArr[i] = arr[i] as Any
             }
             arr = newArr
-            capacity = capacity * 2
-            arr[length] = item
-            length++
+            capacity *= 2
+        }
+        arr[length] = item
+        length++
+
+    }
+
+    fun pop() {
+        if (length > 0) {
+            arr[length - 1] = null
+            length--
         }
     }
 
+    fun printArray() {
+        println(arr.contentToString())
+    }
 
+    fun delete(index: Int) {
+        if (index < 0 || index >= length) throw IndexOutOfBoundsException()
+        for (i in index until length-1) {
+            arr[i] = arr[i + 1]
+        }
+            arr[length-1] = null
+            length--
+        }
+    }
 
-}
 
 
 
